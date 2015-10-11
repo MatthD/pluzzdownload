3289a0b5b330d22ccb756b49ef5720b138410b5a
Matthias <matthias.dieudonne@gmail.com>
2015-10-11 12:16:24 +0200

    Suppresion doubles espaces

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var title,
     date,
     filename;
  
-scrap('http://pluzz.francetv.fr/videos/cash_investigation_,128997113.html', function(err, $) {
+scrap('http://pluzz.francetv.fr/videos/complement_denquete_,128997496.html', function(err, $) {
   var title = $('#diffusion-titre').text(),
-      subtitle = $("h1 .diffusion-paragraphe.soustitre").text().trim(),
+      subtitle = $("h1 .diffusion-paragraphe.soustitre").text().replace(/[^\w\s]/gi, '').trim(),
       date = $("h1").text();
   date = date.replace(title , "").replace(subtitle, "");
   date = exctDate(date);
   date = date[0].day + "-" + date[0].month + "-" + date[0].year;
-  filename = title.replace(/\s/g , "_") + "-" + date + "-" + subtitle.replace(/\s/g , "_") + ".mp4"
+  filename = title.replace(/\s/g , "_") + "-" + date + "-" + subtitle.replace(/\s\s+/g , "_") + ".mp4"
   console.log("Filename" , filename);
 });
 
