c674937b6d2581757c9acdd6ec34c740ddedde11
nCombo <combo.nourdine@gmail.com>
2015-10-23 01:02:18 +0200

    ajout de switch pour convertir la vidéo en fonction du format désir par l'utilisateur

--- middleware/getvideo.js
+++ middleware/getvideo.js
@@ -1,5 +1,5 @@
 /*
-* Télécharge les video de PLUZZ
+* Télécharge les videos de PLUZZ
 */
 
 var avconv = require("avconv"),
@@ -12,7 +12,7 @@ var avconv = require("avconv"),
 
 
 obj.get = function(info,res,io,callback){
-  //Parametres avconv Video/Audio , process.env.HOME correspong à /home/user/
+  //Paramètres avconv Video/Audio , process.env.HOME correspond à /home/user/
 
     if(process.env.HTTP_PROXY){
       proxy = process.env.HTTP_PROXY;
@@ -27,28 +27,45 @@ obj.get = function(info,res,io,callback){
   // Creation du repertoire info.destination s'il il n'existe pas
   mkdirp(info.destination, function (err) {
     if (err) {
-      console.error(kuler("Probléme lors de la création du repertoire : " + err , "red"))
+      console.error(kuler("Problème lors de la création du repertoire : " + err , "red"))
     }
   });
 
-  //Creation de la video dans info.destination
+  //Création de la video dans info.destination
   var stream = avconv(avconv_params);
 
   stream.on('progress', function(progress) {
-    // Affiche le telechargement en cours en % a 2 chiffres prés aprés la virgule
+    // Affiche le téléchargement en cours en % à 2 chiffres près après la virgule
     process.stdout.write(kuler(" ... Téléchargement " + (progress*100).toFixed(2) + "% \r" , "orange"));
-    //Envoi le % de progression à a page html
+    //Envoi le % de progression à la page html
     progress = (progress*100).toFixed(2) + "%";
     io.sockets.emit('update', { progress: progress });
   });
 
   stream.once('exit', function(exitCode, signal, metadata) {
-    io.sockets.emit('update', { toast: "Video Récupérée & Convertie" });
-    res.download(info.destination + info.filename_emission + ".mp4", function(){
-      fs.unlink(info.destination + info.filename_emission + ".mp4");
-    }); // name of archive
+    io.sockets.emit('update', { toast: "Vidéo Récupérée & Convertie" });
+    //conversion en fonction du format de sortie choisi dans index.html
+    switch(caconv_param){
+      case "#formatMKV":
+        res.download(info.destination + info.filename_emission + ".mkv", function(){
+        fs.unlink(info.destination + info.filename_emission + ".mkv");
+        });
+        break;
+      case "#formatMP5":
+        res.download(info.destination + info.filename_emission + ".mp5", function(){
+        fs.unlink(info.destination + info.filename_emission + ".mp5");
+        });
+        break;
+      default "#formatMKV":
+        res.download(info.destination + info.filename_emission + ".mp4", function(){
+        fs.unlink(info.destination + info.filename_emission + ".mp4");
+      });
+    
+    //res.download(info.destination + info.filename_emission + ".mp4", function(){
+    //  fs.unlink(info.destination + info.filename_emission + ".mp4");
+    }; // name of archive
     process.stdout.write('\r');
-    console.log("Video Téléchargée avec succés");
+    console.log("Vidéo Téléchargée avec succès");
   });
 
 };
